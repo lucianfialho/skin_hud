b6cce0d8a65cffc92460ca51b006a550dd1d4e5a
Lucian Fialho <lucianfialhobp@hotmail.com>
2020-10-06 23:21:27 -0300

    -- mockando valores e criando funcao de mudanca de camera

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,66 @@
 <template>
 	<div id="app" class="main" v-if="showSkinHud">
 		<section class="left-tabs">
-		<b-tabs v-model="activeTab">
-			<b-tab-item v-for="tab in components" v-bind:key="tab.id" :label="tab.label">
-				<b-field v-for="property in tab.inputs" v-bind:key="property.id" :label="property.name">
-					<b-slider :min="property.min" :value="property.value" :max="property.max" ticks></b-slider>
-				</b-field>
-			</b-tab-item>
-		</b-tabs>
+			<b-tabs v-model="activeTab">
+				<template v-for="tab in components">
+					<b-tab-item :key="tab.id" :label="tab.label" :value="tab.id" >
+						<template v-for="property in tab.inputs">
+							<b-field :key="property.id" :value="property.id" :label="property.name">
+								<b-slider :min="property.min" :value="property.value" @change="changeCameraOffset(property.camOffset)" :max="property.max" ticks></b-slider>
+							</b-field>
+						</template>
+					</b-tab-item>
+				</template>
+			</b-tabs>
 		</section>
   	</div>
 </template>
 
 <script>
+	const axios = require('axios').default;
 	export default {
 		name: 'app',
 		components: {},
 		data() {
 			return {
+				activeTab: 'body',
 				showSkinHud: false,
 				character: ['sex','face','skin','age_1','age_2','eye_color','beard_1','beard_2','beard_3','beard_4','hair_1','hair_2','hair_color_1','hair_color_2','eyebrows_1','eyebrows_2','eyebrows_3','eyebrows_4','makeup_1','makeup_2','makeup_3','makeup_4','lipstick_1','lipstick_2','lipstick_3','lipstick_4','blemishes_1','blemishes_2','blush_1','blush_2','blush_3','complexion_1','complexion_2','sun_1','sun_2','moles_1','moles_2','chest_1','chest_2','chest_3','bodyb_1','bodyb_2'],
 				components: [
 					{
 						id: 'face',
 						label: 'Feições',
-						inputs: []
+						//inputs: [{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"sex","label":"Sex","max":1,"value":0},{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"face","label":"Face","max":45,"value":0}],
+						inputs: [],
+						camOffset: '0.65'
 					},
 					{
 						id: 'body',
 						label: 'Torso',
-						inputs: []
+						// inputs: [{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"sex","label":"Sex","max":1,"value":0},{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"face","label":"Face","max":45,"value":0}],
+						inputs: [],
+						camOffset: '0.15'
 					},
 					{
 						id: 'legs',
 						label: 'Pernas',
-						inputs: []
+						// inputs: [{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"sex","label":"Sex","max":1,"value":0},{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"face","label":"Face","max":45,"value":0}],
+						inputs: [],
+						camOffset: '-0.5'
 					},
 					{
 						id: 'feets',
 						label: 'Sapatos',
-						inputs: []
+						// inputs: [{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"sex","label":"Sex","max":1,"value":0},{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"face","label":"Face","max":45,"value":0}],
+						inputs: [],
+						camOffset: '-0.8'
 					},
 					{
 						id: 'accessories',
 						label: 'Roupas e acessórios',
-						inputs: []
+						// inputs: [{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"sex","label":"Sex","max":1,"value":0},{"camOffset":0.65,"min":0,"zoomOffset":0.6,"name":"face","label":"Face","max":45,"value":0}],
+						inputs: [],
+						camOffset: '0.75'
 					}
 				]
 			}
@@ -71,7 +87,6 @@
 							if(component.id == 'legs') component.inputs = item.components.filter((element) => (element.camOffset == -0.5 ))
 							if(component.id == 'face') component.inputs = item.components.filter((element) => (element.camOffset == 0.65 && this.character.includes(element.name)))
 							if(component.id == 'body') component.inputs = item.components.filter((element) => (element.camOffset == 0.15 ))
-
 							if(component.id == 'accessories') component.inputs = item.components.filter((element) => (!this.character.includes(element.name)))
 							
 						})
@@ -80,7 +95,15 @@
 				false,
 			);
 		},
-		methods: {},
+		methods: {
+			sendData (name, data) {
+				axios.post(`http://esx_skin_hud/${name}`, {data: data})
+			},
+			changeCameraOffset (camOffset) {
+				this.sendData('esx_skin_hud:ChangeCamera', camOffset)
+			},
+			
+		}
 	};
 </script>
 
@@ -89,7 +112,7 @@
 /* @import 'https://unpkg.com/vue2-animate/dist/vue2-animate.min.css'; */
 html {
 	background: transparent;
-    overflow-y: scroll;
+    overflow-y: hidden;
     width: 30%;
 }
 
